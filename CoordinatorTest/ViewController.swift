41f85b945d4223744f8fa39105eca48dbc186080
Aivars <aivars.meijers@gmail.com>
2018-11-21 03:58:26 +0200

    Initial Commit

---
+++ CoordinatorTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorTest
+//
+//  Created by Aivars Meijers on 21/11/2018.
+//  Copyright © 2018 Aivars Meijers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
